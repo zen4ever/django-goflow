ce35e1b5104307e49df5c9a5c142b11eeacf7d91
esimorre@free.fr <esimorre@free.fr@6b16b2ad-a84a-0410-851a-4d2ecdfd7f99>
2008-04-27 08:49:12 +0000

    graphics2: positioning activities on diagram process works
    git-svn-id: http://goflow.googlecode.com/svn/trunk@4 6b16b2ad-a84a-0410-851a-4d2ecdfd7f99

--- goflow/graphics2/urls_admin.py
+++ goflow/graphics2/urls_admin.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^processimage/pos_activity/$', 'goflow.graphics2.views.pos_activity'),
+    (r'^processimage/(?P<process_id>.*)/pos_activity/$', 'goflow.graphics2.views.pos_activity'),
 )
--- goflow/graphics2/views.py
+++ goflow/graphics2/views.py
@@ -2,7 +2,17 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
-from goflow.workflow.models import Process
+from goflow.graphics2.models import ProcessImage, ActivityPosition
+from goflow.workflow.models import Activity
 
-def pos_activity(request):
-    pass
+def pos_activity(request, process_id):
+    process = ProcessImage.objects.get(id=int(process_id))
+    activity = Activity.objects.get(id=int(request.GET['activity']))
+    activity_pos, created = ActivityPosition.objects.get_or_create(activity=activity, diagram=process)
+    x = int(request.GET['process.x'])
+    y = int(request.GET['process.y'])
+    activity_pos.x = x
+    activity_pos.y = y
+    activity_pos.save()
+    request.user.message_set.create(message='activity %s in positioned in the diagram of process %s.' % (activity.title, process.process.title))
+    return HttpResponseRedirect('..')
